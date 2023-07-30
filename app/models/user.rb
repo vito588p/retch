f7aec732e93b6b8159eb735ddf1b568c27812769
Weber <vito588p@gmail.com>
2023-07-30 21:32:44 +0800

    add password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_create :encrypt_password #Create 前加密密碼
+  
   validates :name, presence: true
   # URI::MailTo::EMAIL_REGEXP 內建email 認證格式
   validates :email, presence: true, unique: true, 
@@ -6,4 +8,9 @@ class User < ApplicationRecord
                       with: URI::MailTo::EMAIL_REGEXP, message: "email格式有霧"
                     }
   validates :password, confirmation: true
+
+  private # 加密密碼，透過雜湊演算 SHA1
+  def encrypt_password
+    self.password = Digest::SHA1.hexdigest(self.password)
+  end
 end
